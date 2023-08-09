cee29a419baa739b0b6e493e068d708ca4f46231
DavidGrossi <david.grossi@pickaform.com>
2023-08-09 23:23:51 +0400

    Updated bundle and made code selectable

--- app.js
+++ app.js
@@ -2337,16 +2337,18 @@ Here are the download links to the resources:
 - <a href="./resources/lib/kissjs/styles/geometry/round.css" download>CSS for round geometry</a>
 
 In case you decide to use the sample **index.html** and sample **index.js** above, please copy all the webfonts and CSS resources inside subfolders, so that your project looks like:
-    **webfonts**/fa-brands-400.woff2
-    **webfonts**/fa-regular-400.woff2
-    **webfonts**/fa-solid-900.woff2
-    **webfonts**/fontawesome-all.min.css
-    **styles**/kissjs.css
-    **styles**/**colors**/light.css
-    **styles**/**geometry**/default.css
+
+    webfonts/fa-brands-400.woff2
+    webfonts/fa-regular-400.woff2
+    webfonts/fa-solid-900.woff2
+    webfonts/fontawesome-all.min.css
+    styles/kissjs.css
+    styles/colors/light.css
+    styles/geometry**/default.css
     index.html
     index.js
     kissjs.min.js
+    
 `
 
 kiss.doc.codingStyle = /*html*/
