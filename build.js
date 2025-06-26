b2d3f116af40832bfcb601803b88517546d4409e
David Grossi <david.grossi@pickaform.com>
2025-06-27 02:58:09 +0400

    Rebuild

--- build.js
+++ build.js
@@ -3427,6 +3427,169 @@ KissJS datatables are really powerful and fast components to display your data.
 })
 
 ;kiss.app.defineView({
+    id: "gallery-content",
+    renderer: function (id, target) {
+        // Build a fake collection
+        let fakeModelTemplate = createFakeModel()
+        fakeModelTemplate.id = "fakeGallery"
+        let fakeModel = new kiss.data.Model(fakeModelTemplate)
+        let fakeCollection = fakeModel.collection
+
+        let fakeAttachmentField = '[{"id":"01887414-3775-7443-81bc-260a9539d7e4","filename":"cyberpunk-cantina-Women-Cyberpunk-ZEN6auDj.png","path":"https://pickaform-europe.s3.eu-west-3.amazonaws.com/files/01844399-988f-7974-a68f-92d35fc702cc/2023/06/01/cyberpunk-cantina-Women-Cyberpunk-ZEN6auDj.png","size":1279664,"type":"amazon_s3","mimeType":"image/png","thumbnails":{"s":{"path":"https://pickaform-europe.s3.eu-west-3.amazonaws.com/files/01844399-988f-7974-a68f-92d35fc702cc/2023/06/01/cyberpunk-cantina-Women-Cyberpunk-ZEN6auDj.64x64.png","size":6106},"m":{"path":"https://pickaform-europe.s3.eu-west-3.amazonaws.com/files/01844399-988f-7974-a68f-92d35fc702cc/2023/06/01/cyberpunk-cantina-Women-Cyberpunk-ZEN6auDj.256x256.png","size":87845},"l":{"path":"https://pickaform-europe.s3.eu-west-3.amazonaws.com/files/01844399-988f-7974-a68f-92d35fc702cc/2023/06/01/cyberpunk-cantina-Women-Cyberpunk-ZEN6auDj.512x512.png","size":332583}},"accessReaders":["*"],"createdAt":"2023-05-31T23:10:59.240Z","createdBy":"david.grossi@pickaform.com"},{"id":"01887414-379e-701e-b4dc-15301d8b4560","filename":"cyberpunk-cantina-Women-Cyberpunk-RUyLqAZl.png","path":"https://pickaform-europe.s3.eu-west-3.amazonaws.com/files/01844399-988f-7974-a68f-92d35fc702cc/2023/06/01/cyberpunk-cantina-Women-Cyberpunk-RUyLqAZl.png","size":1360256,"type":"amazon_s3","mimeType":"image/png","thumbnails":{"s":{"path":"https://pickaform-europe.s3.eu-west-3.amazonaws.com/files/01844399-988f-7974-a68f-92d35fc702cc/2023/06/01/cyberpunk-cantina-Women-Cyberpunk-RUyLqAZl.64x64.png","size":6080},"m":{"path":"https://pickaform-europe.s3.eu-west-3.amazonaws.com/files/01844399-988f-7974-a68f-92d35fc702cc/2023/06/01/cyberpunk-cantina-Women-Cyberpunk-RUyLqAZl.256x256.png","size":89129},"l":{"path":"https://pickaform-europe.s3.eu-west-3.amazonaws.com/files/01844399-988f-7974-a68f-92d35fc702cc/2023/06/01/cyberpunk-cantina-Women-Cyberpunk-RUyLqAZl.512x512.png","size":337313}},"accessReaders":["*"],"createdAt":"2023-05-31T23:10:59.240Z","createdBy":"david.grossi@pickaform.com"},{"id":"01887414-378a-759d-b17d-762b4dd33b72","filename":"cyberpunk-cantina-Women-Cyberpunk-vfNB7pxJ.png","path":"https://pickaform-europe.s3.eu-west-3.amazonaws.com/files/01844399-988f-7974-a68f-92d35fc702cc/2023/06/01/cyberpunk-cantina-Women-Cyberpunk-vfNB7pxJ.png","size":1301698,"type":"amazon_s3","mimeType":"image/png","thumbnails":{"s":{"path":"https://pickaform-europe.s3.eu-west-3.amazonaws.com/files/01844399-988f-7974-a68f-92d35fc702cc/2023/06/01/cyberpunk-cantina-Women-Cyberpunk-vfNB7pxJ.64x64.png","size":6193},"m":{"path":"https://pickaform-europe.s3.eu-west-3.amazonaws.com/files/01844399-988f-7974-a68f-92d35fc702cc/2023/06/01/cyberpunk-cantina-Women-Cyberpunk-vfNB7pxJ.256x256.png","size":89664},"l":{"path":"https://pickaform-europe.s3.eu-west-3.amazonaws.com/files/01844399-988f-7974-a68f-92d35fc702cc/2023/06/01/cyberpunk-cantina-Women-Cyberpunk-vfNB7pxJ.512x512.png","size":331588}},"accessReaders":["*"],"createdAt":"2023-05-31T23:10:59.240Z","createdBy":"david.grossi@pickaform.com"},{"id":"01887414-506f-7707-a529-20a36858b1a8","filename":"cyberpunk-cantina-Women-Cyberpunk-FjFuk2YX.png","path":"https://pickaform-europe.s3.eu-west-3.amazonaws.com/files/01844399-988f-7974-a68f-92d35fc702cc/2023/06/01/cyberpunk-cantina-Women-Cyberpunk-FjFuk2YX.png","size":1357025,"type":"amazon_s3","mimeType":"image/png","thumbnails":{"s":{"path":"https://pickaform-europe.s3.eu-west-3.amazonaws.com/files/01844399-988f-7974-a68f-92d35fc702cc/2023/06/01/cyberpunk-cantina-Women-Cyberpunk-FjFuk2YX.64x64.png","size":6136},"m":{"path":"https://pickaform-europe.s3.eu-west-3.amazonaws.com/files/01844399-988f-7974-a68f-92d35fc702cc/2023/06/01/cyberpunk-cantina-Women-Cyberpunk-FjFuk2YX.256x256.png","size":89761},"l":{"path":"https://pickaform-europe.s3.eu-west-3.amazonaws.com/files/01844399-988f-7974-a68f-92d35fc702cc/2023/06/01/cyberpunk-cantina-Women-Cyberpunk-FjFuk2YX.512x512.png","size":339784}},"accessReaders":["*"],"createdAt":"2023-05-31T23:10:59.240Z","createdBy":"david.grossi@pickaform.com"}]';
+        fakeAttachmentField = JSON.parse(fakeAttachmentField)
+
+        // A kanban needs columns definition.
+        // Here, we use a special method of the model to use the field definitions as columns
+        let columns = fakeModel.getFieldsAsColumns()
+        columns.forEach(column => {
+            column.hidden = !(["category", "description"].includes(column.id))
+        })
+
+        //
+        // Create the gallery
+        //
+        let gallery = createGallery({
+            id: "myGallery",
+            color: "#00aaee",
+            collection: fakeCollection,
+            columns,
+
+            // Options
+            canCreateRecord: true,
+            showImage: true,
+            imageFieldId: "attachment",
+            height: () => kiss.screen.current.height - 50,
+
+            // Define the menu of actions
+            actions: [
+                "-",
+                {
+                    text: "Sort by Category (asc) and Platform (desc)",
+                    icon: "fas fa-sort",
+                    action: () => {
+                        $("myGallery").sortBy([{
+                                category: "asc"
+                            },
+                            {
+                                platform: "desc"
+                            }
+                        ])
+                    }
+                },
+                {
+                    text: "Group by Category and Platform",
+                    icon: "far fa-clone",
+                    action: () => {
+                        $("myGallery").groupBy(["category", "platform"])
+                    }
+                }, {
+                    text: "Add 200 records...",
+                    icon: "fas fa-database",
+                    action: () => {
+                        fakeCollection.hasChanged = true
+                        fakeCollection.insertFakeRecords(200)
+                        createNotification("Records inserted!")
+                        
+                    }
+                }
+            ],
+
+            // openRecord method is triggered when you click at the beginning of a row
+            methods: {
+                selectRecord: async (record) => createForm(record, fakeModel),
+
+                async createRecord(model) {
+                    record = model.create()
+                    const success = await record.save()
+                    if (!success) return
+                    createForm(record)
+                }
+            }
+        })
+
+        return createBlock({
+            id,
+            target,
+
+            style: "user-select: none; background: #ffffff;",
+            height: "100%",
+
+            items: [
+                gallery
+            ],
+
+            methods: {
+                load: () => {
+                    if (fakeCollection.records.length > 0) return
+                    fakeCollection.insertFakeRecords(1000).then(() => {
+                        setTimeout(() => {
+                            fakeCollection.records.forEach(record => record.attachment = fakeAttachmentField)
+                            $("myGallery")._renderDetailsOfVisibleCards()
+                            $("myGallery")._renderGalleryBody()
+                        }, 2000)
+                    })
+                }
+            }
+        })
+    }
+})
+
+;kiss.doc.galleries = /*html*/
+`
+    KissJS galleries are great and simple components to manage your images.
+`
+
+;kiss.app.defineView({
+    id: "gallery-menu",
+    renderer: function (id, target) {
+        return createBlock({
+            id: id,
+            target,
+
+            defaultConfig: {
+                height: 40,
+                textAlign: "left",
+                iconSize: "18px",
+                iconColor: "#8aa2c8",
+                borderColor: "#e3e5ec",
+                borderWidth: "1px 0px 0px 0px",
+                borderRadius: "0px",
+                backgroundColor: "#f3f5f7",
+                colorHover: "#00aaee",
+                iconColorHover: "#00aaee",
+                backgroundColorHover: "#e5e9ec"
+            },
+
+            layout: "vertical",
+            items: [{
+                    type: "html",
+                    html: "Gallery",
+                    class: "navigation-title"
+                },
+                {
+                    type: "button",
+                    text: "Example with 1000 records",
+                    icon: "fas fa-info",
+                    action: () => kiss.router.navigateTo({
+                        anchor: "Introduction about KissJS gallery"
+                    })
+                },
+                {
+                    type: "button",
+                    text: "Back to Home",
+                    icon: "fas fa-arrow-left",
+                    fontWeight: "bold",
+                    action: () => kiss.router.navigateTo({
+                        ui: "start",
+                        section: "home"
+                    })
+                }
+            ]
+        })
+    }
+})
+
+;kiss.app.defineView({
     id: "home-content",
     renderer: function (id, target) {
         return createBlock({
@@ -4038,7 +4201,17 @@ Here is a clean example:
                         section: "timeline",
                         anchor: "Introduction about KissJS timeline"
                     })
-                },                
+                },
+                {
+                    type: "button",
+                    text: "UI: Gallery",
+                    icon: "fas fa-image",
+                    iconColor: "#8c4bff",
+                    action: () => kiss.router.navigateTo({
+                        section: "gallery",
+                        anchor: "Introduction about KissJS gallery"
+                    })
+                },                  
                 {
                     type: "button",
                     text: "Tutorials",
@@ -4747,7 +4920,25 @@ KissJS kanbans are great and simple components to manage your projects and tasks
                                     section: "timeline"
                                 })
                             }
-                        }
+                        },
+                        {
+                            type: "spacer",
+                            width: "2vh"
+                        },
+                        // BUTTON: GALLERY
+                        {
+                            hidden: isMobile,
+                            type: "button",
+                            text: "Gallery example",
+                            backgroundColor: "#9c5940",
+                            backgroundColorHover: "#d87957",
+                            action: () => {
+                                kiss.router.navigateTo({
+                                    ui: "start",
+                                    section: "gallery"
+                                })
+                            }
+                        }                        
                     ]
                 },
                 {
